fc27446d475205510342e065b075a75d1efef0be
Sam Wang <sam.wang.0723@gmail.com>
2018-11-13 17:19:17 +0800

    Parse heroku redis URL

--- utils/redisClient.go
+++ utils/redisClient.go
@@ -2,7 +2,9 @@ package utils
 
 import (
 	"log"
+	"net/url"
 	"os"
+	"strings"
 	"time"
 
 	"github.com/go-redis/redis"
@@ -11,8 +13,17 @@ import (
 var client *redis.Client
 
 func init() {
+	herokuURL := os.Getenv("REDIS_URL")
+	password := ""
+	if !strings.Contains(herokuURL, "localhost") {
+		parsedURL, _ := url.Parse(herokuURL)
+		password, _ = parsedURL.User.Password()
+		herokuURL = parsedURL.Host
+	}
+	log.Printf("Connecting to %s", herokuURL)
 	client = redis.NewClient(&redis.Options{
-		Addr:         os.Getenv("REDIS_URL"),
+		Addr:         herokuURL,
+		Password:     password,
 		DialTimeout:  10 * time.Second,
 		ReadTimeout:  30 * time.Second,
 		WriteTimeout: 30 * time.Second,
