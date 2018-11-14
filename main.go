4ff6843be873be01bd13e4d4a900eabc5ef1971b
Sam Wang <sam.wang.0723@gmail.com>
2018-11-14 10:31:37 +0800

    Remove useless constant

--- main.go
+++ main.go
@@ -23,7 +23,6 @@ import (
 
 const (
 	FACEBOOK_API = "https://graph.facebook.com/v2.6/me/messages?access_token=%s"
-	IMAGE        = "http://37.media.tumblr.com/e705e901302b5925ffb2bcf3cacb5bcd/tumblr_n6vxziSQD11slv6upo3_500.gif"
 )
 
 var currentQueryStoreID string
